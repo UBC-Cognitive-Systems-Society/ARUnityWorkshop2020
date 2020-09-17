7a46240ddf832063bd69f7bd82a22e0c2d40b38e
abelw <unoctium1@gmail.com>
2020-09-17 02:25:06 -0700

    Add models, animations, and starter scripts

---
+++ Assets/Scripts/FollowMousePos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMousePos : MonoBehaviour
+{
+
+    [SerializeField] private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = cam.nearClipPlane;
+        transform.position = cam.ScreenToWorldPoint(mousePos);
+    }
+}
---
+++ Assets/Scripts/Robbie.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Robbie : MonoBehaviour
+{
+
+    Animator animator;
+    public bool isIKActive = true;
+    public bool isTurning = false;
+    [SerializeField] private Transform lookAtObj;
+    [SerializeField] private float waveTime;
+    [SerializeField] private float turnThreshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private IEnumerator OnMouseDown()
+    {
+        animator.SetBool("Wave", true);
+        yield return new WaitForSeconds(waveTime);
+        animator.SetBool("Wave", false);
+    }
+
+    private void Update()
+    {
+        if (!isTurning)
+        {
+            Vector3 relativePos = transform.InverseTransformDirection(lookAtObj.position);
+            if (relativePos.z < turnThreshold)
+            {
+                animator.SetTrigger("TurnAround");
+            }
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (animator)
+        {
+            if (isIKActive)
+            {
+                
+                animator.SetLookAtWeight(1);
+                animator.SetLookAtPosition(lookAtObj.position);
+            }
+            else
+            {
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
